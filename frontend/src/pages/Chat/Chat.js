b8b6400e421e1d6c701daf40d4ad6656502a7058
hsh0321 <ppko1233@gmail.com>
2021-10-21 15:56:41 +0900

    S05P31A102-24 Design : Chatting list UI design update
    * gitmessage 템플릿 추가
    * 컴포넌트, css 추가

--- frontend/src/pages/Chat/Chat.js
+++ frontend/src/pages/Chat/Chat.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ChatList from "./ChatList";
 
 const Chat = () => {
   return (
-    <div>채팅 페이지
-      <Link to={`/tradedetail`}>
+    <div>
+      <ChatList></ChatList>
+      {/* <Link to={`/tradedetail`}>
         <button>디테일</button>
-     </Link>
+      </Link> */}
     </div>
   );
 };
---
+++ frontend/src/pages/ChatRoom/ChatRoom.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ChatRoom() {
+  return <div>asd</div>;
+}
+
+export default ChatRoom;
