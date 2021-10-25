09cc146174bb19a60b0bd62a70d3f33e2ecfb6aa
hsh0321 <ppko1233@gmail.com>
2021-10-25 13:57:52 +0900

    S05P31A102-24 Design : Chatting room UI design update #2

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import NavBottom from "./NavBottom";
 import Side from "./Side";
+import ReserveButton from "../../pages/ChatRoom/ReserveButton";
+import CalendarModal from "../../pages/ChatRoom/CalendarModal";
 
 import { withRouter } from "react-router-dom";
 import "./Layout.css";
 import FloatButton from "./FloatButton";
 
 const Layout = (props) => {
+  const [isOpenReserve, setIsOpenReserve] = useState(false);
+
   return (
     <div className="layout-wrap">
       <div className="layout" id="layout">
@@ -19,6 +23,8 @@ const Layout = (props) => {
         </section>
         <NavBottom location={props.location} />
         <FloatButton location={props.location} />
+        <ReserveButton setOpenReserve={setIsOpenReserve} location={props.location} />
+        <CalendarModal setOpenReserve={setIsOpenReserve} isOpen={isOpenReserve} />
       </div>
       <Side />
     </div>
--- frontend/src/pages/ChatRoom/ChatRoom.js
+++ frontend/src/pages/ChatRoom/ChatRoom.js
@@ -1,86 +1,77 @@
 import React from "react";
 import ChatMessage from "./ChatMessage";
+import CalendarModal from "./CalendarModal";
 
 function ChatRoom({ location }) {
   const msgList = [
     {
+      id: 1,
       type: "message", // 사진, 메시지, 거래 요청
       content: "메시지 내용 1", // 메시지 내용
       time: "오전 10:00", // 메시지 시간
     },
     {
+      id: 2,
       type: "message", // 사진, 메시지, 거래 요청
       content:
         "가나다가나다가나다가나다가나다가나다가나다다가나다가나다가나다가나다가나다가나다가나다가나다", // 메시지 내용
       time: "오전 10:00", // 메시지 시간
     },
     {
+      id: 3,
       type: "message", // 사진, 메시지, 거래 요청
       content: "abcd", // 메시지 내용
       time: "오전 10:00", // 메시지 시간
     },
     {
+      id: 4,
       type: "send", // 사진, 메시지, 거래 요청
       content: "abcd", // 메시지 내용
       time: "오전 10:00", // 메시지 시간
     },
     {
+      id: 5,
       type: "send", // 사진, 메시지, 거래 요청
       content: "하이루", // 메시지 내용
       time: "오전 10:00", // 메시지 시간
     },
     {
+      id: 6,
       type: "message", // 사진, 메시지, 거래 요청
-      content: `달이 익어가니 서둘러 젊은 피야
-        민들레 한 송이 들고
-        사랑이 어지러이 떠다니는 밤이야
-        날아가 사뿐히 이루렴
-        
-        팽팽한 어둠 사이로
-        떠오르는 기분
-        이 거대한 무중력에 혹 휘청해도
-        두렵진 않을 거야
-        
-        푸르른 우리 위로
-        커다란 strawberry moon 한 스쿱
-        나에게 너를 맡겨볼래 eh-oh
-        
-        바람을 세로질러
-        날아오르는 기분 so cool
-        삶이 어떻게 더 완벽해 ooh
-        
-        다시 마주하기 어려운 행운이야
-        온몸에 심장이 뛰어
-        Oh 오히려 기꺼이 헤매고픈 밤이야
-        너와 길 잃을 수 있다면
-        
-        맞잡은 서로의 손으로
-        출입구를 허문
-        이 무한함의 끝과 끝 또 위아래로
-        비행을 떠날 거야
-        
-        푸르른 우리 위로
-        커다란 strawberry moon 한 스쿱
-        나에게 너를 맡겨볼래 eh-oh
-        바람을 세로질러
-        날아오르는 기분 so cool
-        삶이 어떻게 더 완벽해 ooh
-        
-        놀라워 이보다
-        꿈같은 순간이 또 있을까 (더 있을까)
-        아마도 우리가 처음 발견한
-        오늘 이 밤의 모든 것, 그 위로 날아
-        
-        푸르른 우리 위로
-        커다란 strawberry moon 한 스쿱
-        세상을 가져보니 어때 eh-oh
-        
-        바람을 세로질러
-        날아오르는 기분 so cool
-        삶이 어떻게 더 완벽해 ooh`, // 메시지 내용
+      content: `I know that I, I can't believe
+      Just what the past has brought me
+      To the man I wanna be
+      I know that we have had some times
+      That we can't forget the struggle
+      Cause we have so far to go
+      
+      I know we've changed but
+      Change can be so good
+      So let's not forget why
+      It's understood that
+      
+      chorus:
+      Time, look where we are and what we've been through
+      Time, sharing our dreams
+      Time, goes on and on everyday, baby
+      Time is what it is
+      Come what may(come what may)
+      
+      I remember when, mom used to say
+      That things are getting better
+      And you'll soon be on your way
+      Remeber those days
+      When we would sing at the drop of a dime (Oooh)
+      Way back when nothing mattered (Mmm, mm, mm)
+      
+      I know we've changed, but
+      Change can be so good
+      Oh so let's not forget why it's understood that
+      `, // 메시지 내용
       time: "오전 10:00", // 메시지 시간
     },
     {
+      id: 7,
       type: "send", // 사진, 메시지, 거래 요청
       content:
         "하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루하이루", // 메시지 내용
@@ -90,7 +81,7 @@ function ChatRoom({ location }) {
   return (
     <div>
       {msgList.map((msg) => (
-        <ChatMessage profileImg={location.state.profileImg} msg={msg} />
+        <ChatMessage profileImg={location.state.profileImg} msg={msg} key={msg.id} />
       ))}
     </div>
   );
