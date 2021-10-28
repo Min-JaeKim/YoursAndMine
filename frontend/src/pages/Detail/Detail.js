7347232f7445b5a211bcaef2281029ede6003890
hsh0321 <ppko1233@gmail.com>
2021-10-28 15:57:00 +0900

    feat : add item-detail page component # 1
    * 물건 상세 페이지 구현
    	* 이미지 슬라이더
    	* axios 데이터 바인딩

--- frontend/src/pages/Detail/Detail.js
+++ frontend/src/pages/Detail/Detail.js
@@ -6,7 +6,8 @@ import unlikeIcon from "../../assets/icons/wish.png";
 import likeIcon from "../../assets/icons/wish-on.png";
 import { useHistory } from "react-router";
 import { Button } from "semantic-ui-react";
-import axios from "axios";
+import axios from "../../api/axios";
+import Slider from "react-slick";
 
 export const Detail = () => {
   const history = useHistory();
@@ -15,19 +16,27 @@ export const Detail = () => {
   const [loading, setLoading] = useState(true);
   const [like, setLike] = useState(false);
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   useEffect(() => {
     const token = JSON.parse(window.localStorage.getItem("token"));
+    console.log("hi");
     axios
-      .get(`${process.env.REACT_APP_SERVER_BASE_URL}/api/item/detail/${pNo}`, {
+      .get(`/item/${pNo}`, {
         headers: {
           Authentication: "Bearer " + token,
         },
       })
       .then((response) => {
-        setDetail(response.data);
+        setDetail(response.data.item);
         setLoading(false);
-        setLike(response.data.bookmark);
-        console.log(response.data);
+        // setLike(response.data.bookmark);
+        // console.log(response.data);
       })
       .catch((error) => {
         alert("상품 내역이 존재하지 않습니다.");
@@ -90,12 +99,20 @@ export const Detail = () => {
           {/* <Button style={{ backgroundColor: "#497C5F", color: "white" }} className="detail-mayment" onClick={onSelectProduct}>
 								삭제
 							</Button> */}
-          <img src={detail.imgSrc[0]} alt="product" className="detail-product" />
+          <Slider {...settings}>
+            {detail
+              ? detail.itemImage.map((image) => (
+                  <img src={image} alt="product" className="detail-product" />
+                ))
+              : null}
+          </Slider>
+
+          {/* <img src={detail.itemImage[0]} alt="product" className="detail-product" /> */}
           <div className="detail-profile">
-            <img src={detail.owner.image} alt="product" className="detail-user-icon" />
+            <img src={detail.user.userProfileImg} alt="product" className="detail-user-icon" />
             <div className="detail-user-info">
-              <div className="detail-user-name">{detail.owner.username}</div>
-              <div className="detail-user-address">{detail.position}</div>
+              <div className="detail-user-name">{detail.user.userName}</div>
+              <div className="detail-user-address">{detail.user.userAddress}</div>
             </div>
             <div className="detail-like">
               {like ? (
@@ -116,29 +133,27 @@ export const Detail = () => {
               <div>관심 등록</div>
             </div>
           </div>
-          <br />
+          <div className="detail-product-header">
+            <div className="detail-product-name">{detail.itemName}</div>
+            <div className="detail-product-category-time">{detail.itemCategory}</div>
+          </div>
+
           <div className="detail-box">제품 상세</div>
-          <br />
           <div className="detail-product-detail">
-            <div className="detail-product-name">{detail.itemname}</div>
-            <div className="detail-product-category-time">{detail.category}</div>
-            <br />
-            <br />
-            <div>{detail.description}</div>
+            <div>{detail.itemContent}</div>
           </div>
           <div className="detail-inquire-buy">
             <div className="detail-oneday-price">
-              <div className="detail-price">{detail.price} BLI</div>
+              <div className="detail-price">₩ {detail.itemPrice.toLocaleString("ko-KR")}</div>
               <div className="detail-day">1일 기준</div>
             </div>
             <div className="detail-button">
-              <Button style={{ backgroundColor: "#F5F5F5", color: "black" }}>문의하기</Button>
               <Button
                 style={{ backgroundColor: "#497C5F", color: "white" }}
                 className="detail-mayment"
                 onClick={onSelectProduct}
               >
-                대여하기
+                문의하기
               </Button>
             </div>
           </div>
