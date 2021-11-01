76a1b7f3cc166a88c15bc936277e733a907b9978
jinho_pca <jinho_pca@naver.com>
2021-11-01 17:40:16 +0900

    Chore: Add res/req dto 요청, 응답 dto 파일 추가

--- backend/yam/src/main/java/com/ssafy/yam/domain/user/dto/response/UserResponseDto.java
+++ backend/yam/src/main/java/com/ssafy/yam/domain/user/dto/response/UserResponseDto.java
@@ -3,6 +3,7 @@ package com.ssafy.yam.domain.user.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 public class UserResponseDto {
 
@@ -14,5 +15,27 @@ public class UserResponseDto {
         private String accessToken;
         private String refreshToken;
         private Long accessTokenExpiresIn;
+    }
+
+    @Getter
+    @Setter
+    public static class EmailResDto {
+        private String email;
+        private String title;
+        private String message;
+    }
+
+    @Getter
+    @Setter
+    public static class sendEmailResDto {
+        private String certificationNumber;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class modifyProfileResDto {
+        private boolean modifiedImage;
+        private boolean modifiedNickname;
     }
 }
