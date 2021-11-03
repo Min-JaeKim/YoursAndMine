393ee18682057e29d1621796399f5f20f62bf03d
DESKTOP-SKD4R0F\sqk86 <sqk8657@naver.com>
2021-11-03 11:41:07 +0900

    Feat: 웹소켓Config STOMP사용가능하게 수정

--- backend/yam/src/main/java/com/ssafy/yam/config/WebSocketConfig.java
+++ backend/yam/src/main/java/com/ssafy/yam/config/WebSocketConfig.java
@@ -2,20 +2,22 @@ package com.ssafy.yam.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-
+        registry.addEndpoint("/chat")
+                .setAllowedOrigins("http://localhost:8080")
+                .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-
+        registry.setApplicationDestinationPrefixes("/pub")
+                .enableSimpleBroker("/sub");
     }
 }
