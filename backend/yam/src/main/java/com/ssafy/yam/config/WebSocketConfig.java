41288b10a1c06ee2bb80add2173ced13a35660dc
DESKTOP-SKD4R0F\sqk86 <sqk8657@naver.com>
2021-11-04 03:28:51 +0900

    Feat: 웹소켓 컨피그 파일 크로스오리진 *으로 수정

--- backend/yam/src/main/java/com/ssafy/yam/config/WebSocketConfig.java
+++ backend/yam/src/main/java/com/ssafy/yam/config/WebSocketConfig.java
@@ -11,7 +11,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-                .setAllowedOrigins("http://localhost:8080")
+                .setAllowedOrigins("*")
                 .withSockJS();
     }
 
