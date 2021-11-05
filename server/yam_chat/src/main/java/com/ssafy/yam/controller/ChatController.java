0030366997e1e0d84e6fa36f417ecb86ffdca194
DESKTOP-SKD4R0F\sqk86 <sqk8657@naver.com>
2021-11-05 16:16:48 +0900

    Feat: 채팅서버 기능 구현 완료

--- server/yam_chat/src/main/java/com/ssafy/yam/controller/ChatController.java
+++ server/yam_chat/src/main/java/com/ssafy/yam/controller/ChatController.java
@@ -1,28 +1,66 @@
 package com.ssafy.yam.controller;
 
-import com.ssafy.yam.model.ChatMessageDTO;
-import lombok.RequiredArgsConstructor;
+import com.ssafy.yam.model.Conversation;
+import com.ssafy.yam.model.MessageModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
 
 @Controller
-@RequiredArgsConstructor
 public class ChatController {
 
-    private final SimpMessagingTemplate template;
+    private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
 
-    @MessageMapping("/chat/enter")
-    public void enter(ChatMessageDTO messageDTO){
-        System.out.println(messageDTO.getMessage());
-        messageDTO.setMessage(messageDTO.getWriter() + "님이 채팅방에 입장하였습니다.");
-//        template.convertAndSend("/topic/chat/room/" + messageDTO.getRoomdId(), messageDTO);
-        template.convertAndSend("/topic/1", messageDTO);
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @Autowired
+    private RedisTemplate<String, Conversation> conversationTemplate;
+
+    @MessageMapping("/send")
+    public void SendToMessage(MessageModel msg){
+        logger.info("{}", msg);
+        HashOperations<String, String, Conversation> ho = conversationTemplate.opsForHash();
+        if(ho.hasKey(msg.getAuthor(), msg.getTo())){ // 상대방과 대화 데이터가 있을때
+            Conversation con = ho.get(msg.getAuthor(), msg.getTo());
+            con.getMessageList().add(msg);
+            ho.put(msg.getAuthor(),msg.getTo(),con);
+        } else { // 상대방 데이터가 없을때
+            Conversation newCon = new Conversation(msg.getTo(), new ArrayList<>());
+            newCon.getMessageList().add(msg);
+            ho.put(msg.getAuthor(),msg.getTo(),newCon);
+        }
+        if(ho.hasKey(msg.getTo(), msg.getAuthor())){ // 상대방에게 대화 데이터가 있을때
+            Conversation con = ho.get(msg.getTo(), msg.getAuthor());
+            con.getMessageList().add(msg);
+            ho.put(msg.getTo(), msg.getAuthor(),con);
+        } else { // 상대방에게 대화 데이터가 없을때
+            Conversation newCon = new Conversation(msg.getAuthor(), new ArrayList<>());
+            newCon.getMessageList().add(msg);
+            ho.put(msg.getTo(), msg.getAuthor(),newCon);
+        }
+        simpMessagingTemplate.convertAndSend("/topic/"+msg.getTo() , msg);
     }
 
-    @MessageMapping("/chat/message")
-    public void message(ChatMessageDTO messageDTO) {
-        System.out.println(messageDTO.getMessage());
-        template.convertAndSend("/topic/chat/room/" + messageDTO.getRoomdId(), messageDTO);
+    @GetMapping("/fetchAllChats/{userName}")
+    public ResponseEntity<Collection<Conversation>> fetchAll(@PathVariable String userName){
+        HashOperations<String, String, Conversation> hashOperations = conversationTemplate.opsForHash();
+        logger.info("user name: {}", userName);
+        Map<String, Conversation> mapper = hashOperations.entries(userName);
+        Collection<Conversation> res = mapper.values();
+        return new ResponseEntity<>(res, HttpStatus.OK);
     }
 }
