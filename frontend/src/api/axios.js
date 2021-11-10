1cc6aa343f899e25be13fa9a53fc4d69c14ac9dc
sqk8657 <sqk8657@naver.com>
2021-11-10 14:04:49 +0900

    Fix: axios 로그 출력(2)

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import https from "https";
 
 const axiosInstance = axios.create({
   // baseURL: `https://23a41662-eb47-40d9-a8dd-783436980149.mock.pstmn.io/`, 
-  baseURL: `${process.env.REACT_APP_SERVER_BASE_URL}/api`,
+  baseURL: `http://k5a102.p.ssafy.io:8080/api`,
 
   httpsAgent: new https.Agent({ rejectUnauthorized: false }),
 });
