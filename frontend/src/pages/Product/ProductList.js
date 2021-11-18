77ec1cc1178ffcb34c1cfc2cbaca927119bad7f9
yeongeun <ounog@naver.com>
2021-11-18 10:31:10 +0900

    Fix: update Search 검색 css 수정

--- frontend/src/pages/Product/ProductList.js
+++ frontend/src/pages/Product/ProductList.js
@@ -3,18 +3,71 @@ import userActions from "../../redux/actions/userAction";
 import ProductCard from "./ProductCard";
 import axios from "../../api/axios";
 import { useInView } from "react-intersection-observer";
+import Item from "./Item";
+import Loader from "./Loader";
 function ProductList() {
   const [products, setProducts] = useState();
+  const [target, setTarget] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [itemLists, setItemLists] = useState([1]);
 
   useEffect(() => {
-    axios
-      .get(`/item`)
-      .then((response) => {
-        setProducts(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
+      console.log(itemLists);
+      }, [itemLists]);
+
+  const getMoreItem = async () => {
+      setIsLoaded(true);
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      let Items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+      setItemLists((itemLists) => itemLists.concat(Items));
+      setIsLoaded(false);
+  };
+
+  const onIntersect = async ([entry], observer) => {
+      if (entry.isIntersecting && !isLoaded) {
+          observer.unobserve(entry.target);
+          await getMoreItem();
+          observer.observe(entry.target);
+      }
+  };
+
+  useEffect(() => {
+      let observer;if (target) {
+      observer = new IntersectionObserver(onIntersect, {
+          threshold: 0.4,
       });
+      observer.observe(target);
+    }
+    return () => observer && observer.disconnect();
+      }, [target]
+  );
+
+  useEffect(() => {
+    const token = JSON.parse(window.localStorage.getItem("token"));
+    if(token != null && token != ""){
+      axios
+          .get(`/item?page=0&size=12&sort=itemModifiedTime,DESC`, {
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          })
+          .then((response) => {
+            setProducts(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    }else{
+      axios
+          .get(`/item?page=0&size=12&sort=itemModifiedTime,DESC`)
+          .then((response) => {
+            setProducts(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    }
+
   }, []);
 
   return (
@@ -24,6 +77,13 @@ function ProductList() {
             <ProductCard product={product} key={product.itemId}></ProductCard>
           ))
         : null}
+        <div ref={setTarget}>{isLoaded && <Loader />}</div>
+        {/*<AppWrap>*/}
+        {/*    {itemLists.map((v, i) => {*/}
+        {/*        return <Item number={i + 1} key={i} />;*/}
+        {/*    })}*/}
+        {/*    <div ref={setTarget}>{isLoaded && <Loader />}</div>*/}
+        {/*</AppWrap>*/}
     </div>
   );
 }
